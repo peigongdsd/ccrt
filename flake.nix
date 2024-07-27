f5e0aaa1eca067c7905c546654f9aa10324a15e3
Pl Lee <peigong2013@outlook.com>
2024-07-27 23:56:54 +0800

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Common Compact Runtime (ccrt)";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { nixpkgs, self, ... }: let
     builder = nixpkgs.lib.nixosSystem { 
       system = "x86_64-linux"; 
